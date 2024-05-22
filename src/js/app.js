3e6170e4c73f913e6bbbdf2d02e9f043d8fa936b
Ilya Roschikhmarov <ilrosch@mail.ru>
2024-05-22 17:16:06 +0300

    add lessons 2-5 and add prev/next button in header

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as flsFunctions from './modules/functions.js';
 
+import './modules/navigation.js';
 import './modules/slider.js';
+
 // Прочие функции и скрипты
 flsFunctions.isWebp();
 // Прочие функции и скрипты
@@ -11,5 +13,3 @@ const slider = document.querySelector('.lesson-slider');
 
 const img = document.querySelector('.lesson-image');
 img.style.height = `${chess.clientHeight - box.clientHeight - slider.clientHeight - 65}px`;
-
-console.log(chess.clientHeight);
